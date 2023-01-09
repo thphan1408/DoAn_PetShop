3888bc9475f10f834658254595f886c276174b0a
Thanh Phan <th.phan1408@gmail.com>
2023-01-09 18:14:04 +0700

    Add project files.

---
+++ DoAn_PetShop/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_PetShop.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string tk, string mk)
+        {
+            bool kt = (tk.Equals("admin") && mk.Equals("123"));
+            if (kt)
+            {
+                return RedirectToAction("Index", "DashBoard", new { Area = "Admin" });
+            }
+            else
+            {
+                ModelState.AddModelError("", "Tài khoản hoặc mật khẩu sai, vui lòng kiểm tra lại");
+            }
+            return View();
+        }
+    }
+}
